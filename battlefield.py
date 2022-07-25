b32f731fabb524a8f9af722ca255bd000c88afc4
unknown <dylanjlenz@gmail.com>
2022-07-25 15:10:41 -0500

    Modify Robot Class to Create Set Weapon Method.

--- battlefield.py
+++ battlefield.py
@@ -1,3 +1,4 @@
+from dinosaur import Dinosaur
 from herd import Herd
 from fleet import Fleet
 
@@ -12,17 +13,18 @@ class Battlefield:
         Battlefield().battle_phase()
 
     def display_welcome(self):
-        print('\nWelcome Dinosaur VS Robot Arena: \n')
-        
+        print('\nWelcome to the Meat VS Metal Arena: \n')
+    
     def battle_phase(self):
         while self.robot.health and self.dinosaur.health >= 0:
+            self.robot.set_weapon()
             self.robot.attack_dinosaur(self.dinosaur)
-            print(f'{self.dinosaur.name} is hit by {self.robot.name} with an {self.robot.active_weapon.name} for {self.robot.active_weapon.attack_power}, making his health drop to {self.dinosaur.health}.')
+            print(f'{self.dinosaur.name} is hit by {self.robot.name}, with an {self.robot.active_weapon.name} for {self.robot.active_weapon.attack_power}, making his health drop to {self.dinosaur.health}.')
             self.dinosaur.attack_robot(self.robot)
             print(f'{self.robot.name} is hit by {self.dinosaur.name} for {self.dinosaur.attack_power}, making his health drop to {self.robot.health}.\n')
             if self.dinosaur.health <= 1:
                 print(f'The Champion is {self.robot.name}!!\n')
                 return self.robot.name
             elif self.robot.health <= 1:
-                print(f'The Champion is {self.dinosaur.name}\n!!')
+                print(f'The Champion is {self.dinosaur.name}!!\n')
                 return self.dinosaur.name
--- robot.py
+++ robot.py
@@ -6,7 +6,15 @@ class Robot:
     def __init__(self, name):
         self.name = name
         self.health = 150
-        self.active_weapon = Weapon("Axe", random.randint(21, 51))
+               
+    def set_weapon(self):
+        self.active_weapon = input('Robot Weapon Selection: Enter# 1:Laser-Sword; 2:Photon-Baton; 3:Flame-Thrower: \n')
+        if self.active_weapon == '1':
+            self.active_weapon = Weapon("Laser-Sword", random.randint(21, 61))
+        elif self.active_weapon == '2':
+            self.active_weapon = Weapon("Photon-Baton", random.randint(41, 51))
+        elif self.active_weapon == '3':
+            self.active_weapon = Weapon("Flame-Thrower", random.randint(11, 81))
     
     def attack_dinosaur(self, dinosaur):
         dinosaur.health -= self.active_weapon.attack_power
